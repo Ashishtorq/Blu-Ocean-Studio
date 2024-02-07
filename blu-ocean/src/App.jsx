4da0181eb83ad489b371952156a334c6e543a431
Ashistorq <ashishsinghtarwa@gmail.com>
2024-02-07 15:57:09 +0530

    Update code formatting and fix minor bugs

---
+++ blu-ocean/src/App.jsx
@@ -0,0 +1,16 @@
+import { Fragment, useState } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <Fragment>
+      <BrowserRouter>
+        <Routes>
+        <Route></Route>
+        </Routes>
+      </BrowserRouter>
+    </Fragment>
+  );
+}
+
+export default App;
