3582b188d2b50942c3b22239dc62e558db2501c8
Ashistorq <ashishsinghtarwa@gmail.com>
2024-02-07 17:11:11 +0530

    Add external CSS and JavaScript files, update class names and links

---
+++ blu-ocean/index.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  const cursor = document.querySelector(".cursor");
+
+  document.addEventListener("mousemove", function (e) {
+    cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+  });
+});
--- blu-ocean/src/Components/Home/Home.jsx
+++ blu-ocean/src/Components/Home/Home.jsx
@@ -1,7 +1,8 @@
 import React, { Fragment } from "react";
 import "./Home.css";
-// import Link from "react-router-dom"
+
 import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <Fragment>
@@ -9,7 +10,7 @@ const Home = () => {
       <div id="right"></div>
       <div class="cursor"></div>
       <video
-        autoplay
+        autoPlay
         loop
         muted
         src="https://boipl.com/wp-content/uploads/2021/10/size2_new.mp4"
@@ -17,12 +18,57 @@ const Home = () => {
       <div id="main-container">
         <div id="nav-bar">
           <div id="nav-logo">
-        
-              <img
-                src="./logo.png"
-                alt="Blue Ocean Logo"
-              />
-            
+            <img src="./logo.png" alt="Blue Ocean Logo" />
+          </div>
+          <div id="nav-content">
+            <Link to={"./Home.jsx"}>Home</Link>
+            <Link to={"./About.jsx"}>About</Link>
+            <Link to={"./About.jsx"}>OUR INNOVATIONS</Link>
+            <Link to={"./About.jsx"}>OUR IP</Link>
+            <Link to={"./About.jsx"}>STORIES</Link>
+            <Link to={"./About.jsx"}>CAREERS</Link>
+            <Link to={"./About.jsx"}>MEDIA</Link>
+            <i class="ri-more-line" id="dot-icon"></i>
+          </div>
+        </div>
+        <div id="vertical-text">
+          <div id="first-text">
+            <p>
+              Explore <span style={{ fontSize: "40px" }}>ACHIEVEMENTS</span>
+            </p>
+          </div>
+          <div id="second-text">
+            <p>HOW TO FIND US</p>
+            <span style={{ fontSize: "40px" }}>CONTACT US</span>
+          </div>
+        </div>
+        <div id="footer">
+          <div id="social">
+            <Link
+              to={
+                "https://www.facebook.com/Blu-Ocean-Studios-Pvt-Ltd-2173442176025979"
+              }
+            >
+              <i class="ri-facebook-box-fill" id="social-emoji"></i>
+            </Link>
+            <Link
+              to={"https://www.youtube.com/channel/UCWXQZmoJUXeTRpaWxQHsaZQ"}
+            >
+              <i class="ri-youtube-fill" id="social-emoji"></i>
+            </Link>
+            <Link to={"https://www.instagram.com/thekare_/"}>
+              <i class="ri-instagram-fill" id="social-emoji"></i>
+            </Link>
+            <Link
+              to={
+                "https://www.linkedin.com/company/blu-ocean-innovations-pvt-ltd/"
+              }
+            >
+              <i class="ri-linkedin-box-fill" id="social-emoji"></i>
+            </Link>
+          </div>
+          <div id="copy-right">
+            <h6>COPYRIGHT &copy;. ALL RIGHT RESERVED</h6>
           </div>
         </div>
       </div>
